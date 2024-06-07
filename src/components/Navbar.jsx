0a12af53a1e4058fa745c968c8a06f0d139338f3
meghana-wq <meghanakh000@gmail.com>
2024-06-07 16:35:33 +0530

    semi-dynamic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Student app</a>
+                <Link class="navbar-brand" to="#">Student app</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="/">AddStudent</a>
+                      <Link class="nav-link active" aria-current="page" to="/">AddStudent</Link>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/search">SearchStudent</a>
+                    <Link class="nav-link active" aria-current="page" to="/search">SearchStudent</Link>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="/delete">DeleteStudent</a>
+                    <Link class="nav-link" to="/delete">DeleteStudent</Link>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="/view">ViewAllStudents</a>
+                    <Link class="nav-link" to="/view">ViewAllStudents</Link>
                     </li>
                 </ul>
                 </div>
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const ViewAll = () => {
+    const[data,changeData]=useState(
+        [
+        
+            {"Name":"orwel","Admission":111,"Roll":23},
+            {"Name":"megha","Admission":113,"Roll":26},
+            {"Name":"priya","Admission":114,"Roll":28},
+            {"Name":"leya","Admission":114,"Roll":24}
+        ]
+    )
   return (
     <div>
         <Navbar/>
@@ -9,96 +18,19 @@ const ViewAll = () => {
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">ORWEL</li>
-                                    <li class="list-group-item">Football player</li>
-                                    <li class="list-group-item">Thrissur</li>
-                                </ul>
+                        {data.map(
+                            (value,index)=>{
+                                return <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">{value.Name}</li>
+                                        <li class="list-group-item">{value.Admission}</li>
+                                            <li class="list-group-item">{value.Roll}</li>
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">PRIYA</li>
-                                    <li class="list-group-item">Dancer</li>
-                                    <li class="list-group-item">Thrissur</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">MEGHANA</li>
-                                    <li class="list-group-item">Singer</li>
-                                    <li class="list-group-item">Palakad</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">MERCY</li>
-                                    <li class="list-group-item">Dancer</li>
-                                    <li class="list-group-item">Kozhikode</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">NIVEDHYA</li>
-                                    <li class="list-group-item">Cricket player</li>
-                                    <li class="list-group-item">Malappuram</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">POORNIMA</li>
-                                    <li class="list-group-item">Artist</li>
-                                    <li class="list-group-item">Alapuzha</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">SIVA</li>
-                                    <li class="list-group-item">Dancer</li>
-                                    <li class="list-group-item">Palakad</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">RITHWIK</li>
-                                    <li class="list-group-item">Singer</li>
-                                    <li class="list-group-item">TRISSUR</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">MEGHANA</li>
-                                    <li class="list-group-item">Singer</li>
-                                    <li class="list-group-item">Palakad</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                        <div class="card">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">MEGHANA</li>
-                                    <li class="list-group-item">Singer</li>
-                                    <li class="list-group-item">Palakad</li>
-                                </ul>
-                            </div>
-                        </div>
+                            }
+                        )}
                     </div>
                 </div>
             </div>
