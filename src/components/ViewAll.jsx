eec7589c3fffa2020c454c979ff17d9369c582a7
meghana-wq <meghanakh000@gmail.com>
2024-06-07 14:32:26 +0530

    router added

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewAll = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">ORWEL</li>
+                                    <li class="list-group-item">Football player</li>
+                                    <li class="list-group-item">Thrissur</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">PRIYA</li>
+                                    <li class="list-group-item">Dancer</li>
+                                    <li class="list-group-item">Thrissur</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">MEGHANA</li>
+                                    <li class="list-group-item">Singer</li>
+                                    <li class="list-group-item">Palakad</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">MERCY</li>
+                                    <li class="list-group-item">Dancer</li>
+                                    <li class="list-group-item">Kozhikode</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">NIVEDHYA</li>
+                                    <li class="list-group-item">Cricket player</li>
+                                    <li class="list-group-item">Malappuram</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">POORNIMA</li>
+                                    <li class="list-group-item">Artist</li>
+                                    <li class="list-group-item">Alapuzha</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">SIVA</li>
+                                    <li class="list-group-item">Dancer</li>
+                                    <li class="list-group-item">Palakad</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">RITHWIK</li>
+                                    <li class="list-group-item">Singer</li>
+                                    <li class="list-group-item">TRISSUR</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">MEGHANA</li>
+                                    <li class="list-group-item">Singer</li>
+                                    <li class="list-group-item">Palakad</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">MEGHANA</li>
+                                    <li class="list-group-item">Singer</li>
+                                    <li class="list-group-item">Palakad</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewAll
